74984647180a489a315de70a62389eac9474d88f
Gabriel Paladino <gabriel.paladino@stormsec.com.br>
2018-02-26 14:16:27 -0300

    tags

--- tags.py
+++ tags.py
@@ -5,17 +5,21 @@ from subprocess import call
 
 
 def run_ctags(exclude_dir):
-    cmd0 = "rm tags"
-    print('>>>', cmd0)
-    call(cmd0, shell=True)
+    cmd = "rm -rf .tox"
+    print('>>>', cmd)
+    call(cmd, shell=True)
 
-    cmd1 = "ctags -R --exclude='*/{}' $VIRTUAL_ENV/".format(exclude_dir)
-    print('>>>', cmd1)
-    call(cmd1, shell=True)
+    cmd = "rm tags"
+    print('>>>', cmd)
+    call(cmd, shell=True)
 
-    cmd2 = "ctags -R --exclude=.tox -a -o tags ."
-    print('>>>', cmd2)
-    call(cmd2, shell=True)
+    cmd = "ctags -R --exclude='*/{}' $VIRTUAL_ENV/".format(exclude_dir)
+    print('>>>', cmd)
+    call(cmd, shell=True)
+
+    cmd = "ctags -R --exclude=.tox -a -o tags ."
+    print('>>>', cmd)
+    call(cmd, shell=True)
 
 
 if __name__ == '__main__':
