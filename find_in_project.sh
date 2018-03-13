ef88c47ab6a5d87b4390a660df15acbf230027db
Gabriel Paladino <l31rb4g@gmail.com>
2018-03-13 09:18:51 -0300

    find

--- find_in_project.sh
+++ find_in_project.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 xdotool key "control+shift+e"
-
 #xdotool type 'ag --hidden --pager "less -r" "'$1'"'
-#xdotool type 'echo $$ > /tmp/.vim_find'
 #xdotool key KP_Enter
-
 sleep 0.5
 
 pid=$(ps au --sort=-start_time | grep bash | grep -v grep | head -n 2 | tail -n 1 | sed 's/ \+/ /g' | cut -d ' ' -f 2)
-#pid=$(cat /tmp/.vim_find)
-
 #echo 'PID: '$pid
 
 if [ $pid ]; then
@@ -18,5 +13,6 @@ if [ $pid ]; then
     clear
     echo "Searching for $1" > $fd
     echo "" > $fd
-    (ag --hidden "$1" > $fd) &
+    (ag --hidden --ignore tags "$1" > $fd)& 
 fi
+
