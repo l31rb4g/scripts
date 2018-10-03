4e209f68f3dae5c8a61c25ff6319687b5b24e158
Gabriel Paladino <l31rb4g@gmail.com>
2018-10-03 19:03:21 -0300

    timebox

--- timebox.py
+++ timebox.py
@@ -9,6 +9,9 @@ TMP_FILE = '/tmp/timebox'
 DEFAULT_TIME = 15
 DEBUG = False
 
+
+null = open('/dev/null')
+
 def shell(cmd:list):
     return check_output(cmd).decode().strip('\n')
 
@@ -22,52 +25,53 @@ def cancel():
         os.remove(TMP_FILE)
 
 def kill():
-    call(['killall', 'timebox'])
+    call(['killall', 'timebox'], stderr=null)
 
 
 
 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--cancel':
+            kill()
+            cancel()
+            sys.exit()
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == '--cancel':
-        kill()
-        cancel()
-        sys.exit()
+        time = int(sys.argv[1])
 
-    time = int(sys.argv[1])
-
-else:
-    time = shell(['zenity', '--entry',
-                      '--text=Digite o tempo (em minutos):',
-                      '--title=Timebox',
-                      '--entry-text=' + str(DEFAULT_TIME)])
-    if time in ['c', 'cancel']:
-        kill()
-        cancel()
-        sys.exit()
     else:
-        time = int(time)
+        time = shell(['zenity', '--entry',
+                          '--text=Digite o tempo (em minutos):',
+                          '--title=Timebox',
+                          '--entry-text=' + str(DEFAULT_TIME)])
+        if time in ['c', 'cancel']:
+            kill()
+            cancel()
+            sys.exit()
+        else:
+            time = int(time)
 
 
-kill()
-cancel()
+    # kill()
+    cancel()
 
-s = '' if time == 1 else 's'
-debug('Timebox iniciado com duração de {} minuto{}.'.format(time, s))
+    s = '' if time == 1 else 's'
+    debug('Timebox iniciado com duração de {} minuto{}.'.format(time, s))
 
-remaining = time
-for i in range(time):
-    debug(remaining)
+    remaining = time
+    for i in range(time):
+        debug(remaining)
+        with open(TMP_FILE, 'w') as f:
+            f.write(str(remaining))
+        remaining -= 1
+        sleep(1 if DEBUG else 60)
+
     with open(TMP_FILE, 'w') as f:
-        f.write(str(remaining))
-    remaining -= 1
-    sleep(1 if DEBUG else 60)
+        f.write("0")
 
-with open(TMP_FILE, 'w') as f:
-    f.write("0")
+    debug('Timebox finalizado!')
+    cancel()
 
-debug('Timebox finalizado!')
-cancel()
+    call(['cvlc', '--play-and-exit', SOUND], stderr=null)
 
-call(['cvlc', '--play-and-exit', SOUND], stderr=open('/dev/null'))
-
+    null.close()
