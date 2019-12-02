74a0e779003ded16c1734bc5ac0e8e92c20005b7
Gabriel Paladino <l31rb4g@gmail.com>
2019-12-02 10:45:06 -0300

    go

--- chroma.sh
+++ chroma.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-CAMERA='/dev/video1'
-DUMMY='/dev/video0'
-VIDEO_BASE=~/Downloads/praia2.mp4
+CAMERA='/dev/video0'
+DUMMY='/dev/video2'
+VIDEO_BASE=~/chroma-videos/floresta.mp4
 
-COLOR='7ACFAE' # com luz
+#COLOR='7ACFAE' # com luz
 #COLOR='95B6A1' # sem luz
 #COLOR='3B6767' # verde escuro
+COLOR='64CF6C' # novo verde
 
-SIMILARITY='0.21'
-BLEND='0.21'
+SIMILARITY='0.19'
+BLEND='0.2'
 
 
 ffmpeg -y \
     -stream_loop -1 \
     -i $VIDEO_BASE \
     -i $CAMERA \
-    -filter_complex '[1:v]crop=1150:720:50:0[crop];[crop]colorkey=0x'$COLOR':'$SIMILARITY':'$BLEND'[ckout];[0:v][ckout]overlay[out]' \
+    -filter_complex '[1:v]crop=1100:720:100:0[crop];[crop]colorkey=0x'$COLOR':'$SIMILARITY':'$BLEND'[ckout];[0:v][ckout]overlay[out]' \
     -map '[out]' \
     -f v4l2 \
     $DUMMY
